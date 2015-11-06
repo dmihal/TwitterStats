8b70580ae63fa6f5e434e0dc510d332e2091626b
David Mihal <eastsideski@gmail.com>
2015-11-06 11:23:09 -0500

    Add user model

---
+++ model/user.jsx
@@ -0,0 +1,17 @@
+Users = new Mongo.Collection('users');
+
+var userSchema = new SimpleSchema({
+  twitter_id: {
+    type: Number,
+    label: "User ID from Twitter",
+    min: 0
+  },
+  screen_name: {
+    type: String,
+    label: "The twitter @handle",
+    regEx: /^[A-Za-z0-9_]{1,15}$/
+  },
+  last_updated: {
+    type: Date
+  }
+});
