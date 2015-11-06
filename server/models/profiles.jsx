bf79dec23fb684d9338df20a3e4f7afca7e99d5c
David Mihal <eastsideski@gmail.com>
2015-11-06 18:39:12 -0500

    Refresh data if it's been at least 30 minutes and it's reviewed

--- server/models/profiles.jsx
+++ server/models/profiles.jsx
@@ -3,10 +3,15 @@ Meteor.publish("profile", function (screen_name) {
   var dataSearch = DataPoints.find({screenname: screen_name});
 
   var collections = [profilesSearch, dataSearch];
-  if (profilesSearch.count() > 0){
-    return collections;
-  } else {
+  if (profilesSearch.count() == 0 || dataSearch.count() == 0){
     TwitterHandler.enqueue(screen_name);
-    return collections;
+  } else {
+    var recentDP = DataPoints.findOne({screenname: screen_name}, {sort: {date: -1}});
+    var diff = moment().diff(moment(recentDP.date), 'minutes');
+    console.log("Difference of", diff, "minutes");
+    if (diff > 30){
+      TwitterHandler.enqueue(screen_name);
+    }
   }
+  return collections;
 });
